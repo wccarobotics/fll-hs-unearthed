e843265eba35ab3d0b7480bc67c2e63b452df068
Lucas Plaisted <lsplaisted@outlook.com>
2026-03-28 23:20:33 -0400

    Improve mission 6/7 reliability

--- code/hs67.py
+++ code/hs67.py
@@ -6,9 +6,6 @@ from pybricks.hubs import PrimeHub
 import robot
 
 def Run(drive_base: DriveBase, left_attachment: Motor, right_attachment: Motor, prime_hub: PrimeHub):
-
-
-    # The main program starts here.
     drive_base.use_gyro(True)
     wait(200)
     # start on back line near mission 11
@@ -26,17 +23,19 @@ def Run(drive_base: DriveBase, left_attachment: Motor, right_attachment: Motor, 
     drive_base.turn(-45)
     drive_base.straight(60)
     drive_base.settings(straight_speed=200)
-    drive_base.turn(-45)
+    drive_base.turn(-30)
+    drive_base.straight(-60)
+    drive_base.turn(-15)
     # Leaving missions
     drive_base.settings(straight_speed=460)
-    drive_base.straight(440)
+    drive_base.straight(500)
     # tern torward scale
-    drive_base.settings(straight_speed=400)
+    drive_base.settings(straight_speed=900)
     drive_base.turn(-90)
     # Doing mission 10
     drive_base.settings(straight_speed=150)
-    drive_base.straight(-160)
-    drive_base.turn(-45)
+    drive_base.straight(-155)
+    drive_base.turn(-48)
     drive_base.settings(straight_speed=700)
     drive_base.straight(340)
     drive_base.straight(-200)
@@ -44,12 +43,14 @@ def Run(drive_base: DriveBase, left_attachment: Motor, right_attachment: Motor, 
     drive_base.settings(straight_speed=900)
     drive_base.straight(350)
     right_attachment.run_angle(500, 190)
-    drive_base.turn(-15)
+    drive_base.turn(-18)
     drive_base.straight(300, then=Stop.COAST)
     drive_base.straight(-300)
     drive_base.turn(78)
     drive_base.settings(straight_speed=900)
     drive_base.straight(650)
+    drive_base.settings(straight_speed=200)
+
 
 
 # This code allows this program to be run directly, without the main program
