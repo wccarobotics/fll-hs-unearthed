69fc8f8edf5d8b3eeac380e8c424502c98aa9c36
Lucas Plaisted <lsplaisted@outlook.com>
2026-05-01 16:00:54 -0400

    Push boulders out

--- hs67.py
+++ hs67.py
@@ -7,7 +7,6 @@ import robot
 
 def Run(drive_base: DriveBase, left_attachment: Motor, right_attachment: Motor, prime_hub: PrimeHub):
     drive_base.use_gyro(True)
-    wait(200)
     # start on back line near mission 11
     # bottom left of yellow box
     # wheel on the 3rd line from the big black
@@ -41,15 +40,10 @@ def Run(drive_base: DriveBase, left_attachment: Motor, right_attachment: Motor, 
     drive_base.straight(-200)
     drive_base.turn(-22.5, then=Stop.NONE)
     drive_base.settings(straight_speed=900)
-    drive_base.straight(350)
-    drive_base.turn(50)
-    drive_base.settings(straight_speed=900)
-    drive_base.straight(600)
-
-
-
-
-# This code allows this program to be run directly, without the main program
-if __name__ == "__main__":
-    robot = robot.Robot()
-    Run(robot.drive_base, robot.left_attachment, robot.right_attachment)
+    drive_base.straight(10, then=Stop.NONE)
+    right_attachment.run_angle(500, 360)
+    drive_base.straight(250, then=Stop.NONE)
+    drive_base.arc(-150, angle=17, then=Stop.COAST)
+    drive_base.straight(50, then=Stop.NONE)
+    right_attachment.run_angle(500, -360)
+    drive_base.straight(150)
